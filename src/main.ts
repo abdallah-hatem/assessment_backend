29068da2f86aaf389a67aea8b5e7d6be080c10ef
abdallah-hatem <ahkortam@gmail.com>
2023-11-22 22:39:31 +0200

    added the deployed frontend url to Cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://assessment-frontend-eight.vercel.app',
+    ],
   });
 
   await app.listen(8000);
